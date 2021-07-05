4fdf2752cb200f49b99fb4cc717e2d55474db5ed
grillow <spmr757@gmail.com>
2021-07-05 15:45:10 +0300

    more meta + linux kernel style

--- 1.2/main.c
+++ 1.2/main.c
@@ -2,14 +2,20 @@
 #include <linux/kernel.h>
 
 
+MODULE_AUTHOR("grillow1337");
+MODULE_DESCRIPTION("ADC_Test");
+MODULE_VERSION("1.0.0");
 MODULE_LICENSE("GPL");
 
-int init_module() {
+
+int init_module()
+{
     printk(KERN_INFO "ADC_Test started!\n");
     return 0;
 }
 
 
-void cleanup_module() {
+void cleanup_module()
+{
     printk(KERN_INFO "ADC_Test stopped!\n");
 }
