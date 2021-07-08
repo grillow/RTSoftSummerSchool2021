b67b237ec3b4b1bc877dfeeffd5ff8a6531a6acb
grillow <spmr757@gmail.com>
2021-07-08 19:36:38 +0300

    2.2 done

--- 2.2/src/main.cpp
+++ 2.2/src/main.cpp
@@ -1,42 +1,35 @@
+#include <opencv2/core/types.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
-
-
-void recogniseStickersByThreshold(cv::Mat image,std::vector<cv::Mat> &stickers,
-        const double key_light = 100, const double key_sat = 100, const double key_hue = 100) {
-    
-    cv::Mat image_hsv;
-    std::vector< std::vector<cv::Point> > contours;
-    cv::cvtColor(image, image_hsv, cv::COLOR_BGR2HSV ); // Преобразуем в hsv
-    cv::Mat tmp_img(image.size(),CV_8U);
-    // Выделение подходящих по цвету областей. Цвет задается константой :)
-    cv::inRange(image_hsv,
-            cv::Scalar(key_light-2,key_sat-20,key_hue-35),
-            cv::Scalar(key_light+2,key_sat+20,key_hue+35),
-            tmp_img);
-    // "Замазать" огрехи в при выделении по цвету
-    cv::dilate(tmp_img,tmp_img,cv::Mat(),cv::Point(-1,-1),3);
-    cv::erode(tmp_img,tmp_img,cv::Mat(),cv::Point(-1,-1),1);
-    //Выделение непрерывных областей
-    cv::findContours(tmp_img,contours,CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-    for (uint i = 0; i<contours.size(); i++) {
-        cv::Mat sticker;
-        //Для каждой области определяем ограничивающий прямоугольник
-        cv::Rect rect=cv::boundingRect(contours[i]);
-        image(rect).copyTo(sticker);
-        stickers.push_back(sticker); //Добавить к массиву распознанных стикеров
-    }
-}
+#include <vector>
 
 
 auto main() -> int {
-    cv::VideoCapture cap("data/video.mov");
-    if(!cap.isOpened()) {
-        return -1;
-    }
-    
+    cv::VideoCapture cap("data/video.mkv");
     cv::Mat frame;
     while(cap.read(frame)) {
-        imshow("window", frame);
+        cv::Mat processed(frame);
+
+        cv::Mat gray;
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+
+        cv::GaussianBlur(gray, gray, { 7, 7 }, 7);
+
+        cv::Mat edged;
+        cv::Canny(gray, edged, 60, 180);
+
+
+        std::vector<std::vector<cv::Point> > contours;
+        std::vector<cv::Vec4i> hierarchy;
+        findContours(edged, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+
+
+        for (const auto & contour : contours) {
+            const cv::Rect rect = cv::boundingRect(contour);
+            cv::rectangle(processed, rect, { 255, 0, 0 }, 1);
+        }
+
+        imshow("window", processed);
         if(cv::waitKey(30) >= 0) break;
     }
 
