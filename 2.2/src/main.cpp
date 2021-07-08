0eb5b993397ca2e4acde8e5c1b5d69c8eb1e75e0
grillow <spmr757@gmail.com>
2021-07-08 16:24:07 +0300

    more advanced setup

--- 2.2/src/main.cpp
+++ 2.2/src/main.cpp
@@ -1,33 +1,44 @@
-#if 0
-#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+void recogniseStickersByThreshold(cv::Mat image,std::vector<cv::Mat> &stickers,
+        const double key_light = 100, const double key_sat = 100, const double key_hue = 100) {
+    
+    cv::Mat image_hsv;
+    std::vector< std::vector<cv::Point> > contours;
+    cv::cvtColor(image, image_hsv, cv::COLOR_BGR2HSV ); // Преобразуем в hsv
+    cv::Mat tmp_img(image.size(),CV_8U);
+    // Выделение подходящих по цвету областей. Цвет задается константой :)
+    cv::inRange(image_hsv,
+            cv::Scalar(key_light-2,key_sat-20,key_hue-35),
+            cv::Scalar(key_light+2,key_sat+20,key_hue+35),
+            tmp_img);
+    // "Замазать" огрехи в при выделении по цвету
+    cv::dilate(tmp_img,tmp_img,cv::Mat(),cv::Point(-1,-1),3);
+    cv::erode(tmp_img,tmp_img,cv::Mat(),cv::Point(-1,-1),1);
+    //Выделение непрерывных областей
+    cv::findContours(tmp_img,contours,CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+    for (uint i = 0; i<contours.size(); i++) {
+        cv::Mat sticker;
+        //Для каждой области определяем ограничивающий прямоугольник
+        cv::Rect rect=cv::boundingRect(contours[i]);
+        image(rect).copyTo(sticker);
+        stickers.push_back(sticker); //Добавить к массиву распознанных стикеров
+    }
+}
 
 
 auto main() -> int {
-    
-    return 0;
-}
-#endif
-
-#include "opencv2/opencv.hpp"
-using namespace cv;
-int main(int, char**) {
-    VideoCapture cap(0); // Открываем камеру или файл
-    if(!cap.isOpened())
-        // check if we succeeded
+    cv::VideoCapture cap("data/video.mov");
+    if(!cap.isOpened()) {
         return -1;
-    Mat edges;
-    namedWindow("edges",1);
-    while(1) {
-        Mat frame;
-        cap >> frame; // get a new frame from camera
-        cvtColor(frame, edges, COLOR_BGR2GRAY);
-        // Перевод в градации серого
-        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5); // Размытие
-        Canny(edges, edges, 0, 30, 3);
-        // Выделение границ
-        imshow("edges", edges);
-        if(waitKey(30) >= 0) break;
     }
+    
+    cv::Mat frame;
+    while(cap.read(frame)) {
+        imshow("window", frame);
+        if(cv::waitKey(30) >= 0) break;
+    }
+
     return 0;
 }
-// Пример из http://docs.opencv.org/3.0-beta/modules/videoio/doc/reading_and_writing_video.html
